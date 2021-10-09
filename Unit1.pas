3bd1bd308fc97dcc81089b11b746aa132bbb57ba
Yuri Sanford <75326223+sanfordyuri@users.noreply.github.com>
2021-10-09 17:21:15 -0300

    Design telaPrincipal Concluido.

---
+++ Unit1.pas
@@ -0,0 +1,33 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Edit1: TEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    tempoComboBox: TComboBox;
+    Label5: TLabel;
+    rodarButton: TButton;
+    Button2: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
